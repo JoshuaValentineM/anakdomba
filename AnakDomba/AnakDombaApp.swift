2b30b3d16ac6163341584363fa5d799c28a4d873
JoshuaValentineM <joshuavalentinem@gmail.com>
2026-04-10 08:51:50 +0700

    Initial Commit

---
+++ AnakDomba/AnakDombaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnakDombaApp.swift
+//  AnakDomba
+//
+//  Created by Joshua Valentine Manik on 10/04/26.
+//
+
+import SwiftUI
+
+@main
+struct AnakDombaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnakDomba/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AnakDomba
+//
+//  Created by Joshua Valentine Manik on 10/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
